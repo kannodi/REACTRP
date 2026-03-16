61e5bd44b9fde00dd96540d3d4d78c670812d6e8
kannodi <okihnnj123@gmail.com>
2026-03-16 14:25:06 -0500

    Bloque C — PlatoCard en su lugar, Home conectado, hot reload verificado

--- DIA2/src/pages/Home.jsx
+++ DIA2/src/pages/Home.jsx
@@ -1,16 +1,24 @@
+import { platosMock } from '../data/platos.mock.js'
+import PlatoCard from '../components/PlatoCard'
+
 function Home() {
-    return (
-        <>
-            <div>
-                <h1>Carta del Restaurante</h1>
-                <p>Aqui van los platos</p>
-                <ul>
-                    <li>Aji de Pollo</li>
-                    <li>Lomo Saltado</li>
-                    <li>Ceviche</li>
-                </ul>
-            </div>
-        </>
-    )
+  return (
+    <div>
+      <h1>Carta del Restaurante</h1>
+      <ul>
+        {platosMock.map(plato => (
+          <PlatoCard
+            key={plato.id}
+            nombre={plato.nombre}
+            categoria={plato.categoria}
+            precio={plato.precio}
+            stock={plato.stock}
+            disponible={plato.disponible}
+          />
+        ))}
+      </ul>
+    </div>
+  )
 }
+
 export default Home
