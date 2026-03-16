7a04a3581d6a4f40d6f3b87eea0f7e1c9fa41e2c
JoelASB <joelasantivab@gmail.com>
2026-03-16 13:17:43 -0500

    Bloque B — estructura de carpetas del restaurante creada

---
+++ DIA2/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+function Home() {
+    return (
+        <>
+            <div>
+                <h1>Carta del Restaurante</h1>
+                <p>Aqui van los platos</p>
+                <ul>
+                    <li>Aji de Pollo</li>
+                    <li>Lomo Saltado</li>
+                    <li>Ceviche</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+export default Home
