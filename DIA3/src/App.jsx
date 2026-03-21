1e403dcd7d80eb6e8988a3267ca5b4e88cd24b80
JoelASB <joelasantivab@gmail.com>
2026-03-21 13:55:47 -0500

    NavBar - 1

--- DIA3/src/App.jsx
+++ DIA3/src/App.jsx
@@ -1,9 +1,10 @@
 import Home from './pages/Home'
 import MesasPage from './pages/MesasPage'
-
+import NavBar from './components/NavBar'
 function App() {
   return (
     <div>
+      <NavBar />
       <Home />
       <MesasPage />
     </div>
---
+++ DIA3/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+
+function NavBar() {
+    return (
+        <>
+            <header>
+                <h1>Restaurante</h1>
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="#">Carta</a></li>
+                    <li><a href="#">Mesas</a></li>
+                    <li><a href="#">Comandas</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
