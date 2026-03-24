1b786054e5636c20d1795cc6a6bdafa5478f0766
kannodi <okihnnj123@gmail.com>
2026-03-24 13:16:28 -0500

    Plantilla Rapida Dia 4

--- DIA4/src/App.jsx
+++ DIA4/src/App.jsx
@@ -1,12 +1,14 @@
 import Home from './pages/Home'
 import MesasPage from './pages/MesasPage'
 import NavBar from './components/NavBar'
+import ComandasPages from './pages/ComandasPage'
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
       <MesasPage />
+      <ComandasPages />
     </div>
   )
 }
