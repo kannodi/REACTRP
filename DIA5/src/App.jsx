3ded469efca5cbdb28ad0238dd122fc1512c653b
kannodi <okihnnj123@gmail.com>
2026-03-24 13:21:56 -0500

    Dia 5 Plantilla Inicial

---
+++ DIA5/src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './pages/Home'
+import MesasPage from './pages/MesasPage'
+import NavBar from './components/NavBar'
+import ComandasPages from './pages/ComandasPage'
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <MesasPage />
+      <ComandasPages />
+    </div>
+  )
+}
+
+export default App
