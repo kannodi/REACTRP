6aeec3e94d5095942797c4f9ded6d4b35e66fabc
Paolo Lopez <okihnnj123@gmail.com>
2026-04-08 11:23:00 -0500

    Agregando Pagina no Encontrada 404

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,23 @@ import CartaPage from './pages/CartaPage'
 import ComandasPages from './pages/ComandasPage'
 import MesasPage from './pages/MesasPage'
 import DetalleMesa from './pages/DetallesMesa'
-
+import NotFound from './pages/NotFound'
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        {/* Ruta por defecto */}
+        {/* Ruta por defecto enviara al login*/}
         <Route path="/" element={<Navigate to='/login' replace />} />
+        {/*Rutas protegidas*/}
         <Route path="/login" element={<><LoginPage /></>} />
         <Route path="/menu" element={<ProtectedRoute><MenuPage /></ProtectedRoute>} />
         <Route path="/comandas" element={<ProtectedRoute><ComandasPages /></ProtectedRoute>} />
         <Route path="/carta" element={<ProtectedRoute><CartaPage /></ProtectedRoute>} />
         <Route path="/mesas" element={<ProtectedRoute><MesasPage /></ProtectedRoute>} />
         <Route path='/mesas/:id' element={<ProtectedRoute><DetalleMesa /></ProtectedRoute>} />
-
+        {/*Ruta en caso de que no haya pagina*/}
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
