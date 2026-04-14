57522aef834ab2fdfcca5d5d06fc240a112bb541
Paolo Lopez <okihnnj123@gmail.com>
2026-04-14 08:15:15 -0500

    limpiar

--- src/components/PlatoCard.jsx
+++ src/components/PlatoCard.jsx
@@ -8,26 +8,28 @@ function PlatoCard({ plato }) {
       <div className='flex justify-between items-start'>
         <h3 className='font-bold text-gray-800 text-lg '>{plato.nombre}</h3>
         <span className='text-green-600 font-semibold text-lg'>S/ {plato.precio}</span>
-
       </div>
-      <span className='text-xs bg-blue-100 text-blue-700 px-2 py-1 rounded-full w-fit'>
-        {plato.categoria}
-      </span>
-
-      <button
-        onClick={() => agregarPlato(plato)}
-        className='justify-end mt-2 w-fit bg-yellow-500 text-white py-1 rounded hover:bg-yellow-600 p-4'>
-        + Agregar a comanda
-      </button>
-      <div className='flex justify-between items-center mt-1'>
-        <span className='text-gray-400 text-sm'>Stock: {plato.stock}</span>
-        <span className={`text-xs font-medium px-2 py-1 rounded-full ${plato.stock > 0
+      <div>
+        <span className={`text-xs font-medium px-2 py-1 rounded-lg ${plato.stock > 0
           ? 'bg-green-100 text-green-700'
           : 'bg-red-100 text-red-500'
           }`}>
           {plato.stock > 0 ? '✅ Disponible' : '❌ Agotado'}
         </span>
+        <span className='ml-2 text-xs bg-blue-100 text-blue-700 px-2 py-1 rounded-lg w-fit'>
+          {plato.categoria}
+        </span>
+
       </div>
+      <div className='flex justify-between items-center mt-1'>
+        <span className='text-gray-400 text-sm'>Stock: {plato.stock}</span>
+
+      </div>
+
+      <button onClick={() => agregarPlato(plato)}
+        className='justify-end mt-2 w-full bg-yellow-500 text-white font-bold py-1 rounded hover:bg-yellow-600 p-4'>
+        Agregar a comanda
+      </button>
 
     </div>
 
--- src/pages/CartaPage.jsx
+++ src/pages/CartaPage.jsx
@@ -1,15 +1,11 @@
 import { useState, useEffect } from 'react';
 import PlatoCard from '../components/PlatoCard';
 import { getPlatos } from '../services/api';
-import { usePedido } from '../context/PedidoContext';
 
 function CartaPage() {
   const [platos, setPlatos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const { pedido } = usePedido();
-  const totalItems = pedido.items.reduce((acc, i) => acc + i.cantidad, 0);
 
   useEffect(() => {
     async function cargarPlatos() {
@@ -31,24 +27,15 @@ function CartaPage() {
 
   return (
     <div className='m-10'>
-      <h1 className="bg-blue-400 text-white text-2xl font-bold p-2 rounded-xl mb-2">Carta del Restaurante</h1>
-      <ul>
+      <h1 className="bg-blue-400 text-white text-2xl font-bold p-2 rounded-xl mb-2 ">Carta del Restaurante</h1>
+      <div className=' flex flex-col grid grid-cols-4 gap-1'>
         {platos.map(plato => (
           <PlatoCard
             key={plato._id}
             plato={plato}
           />
         ))}
-      </ul>
-      <nav>
-        {/* Badge visible en MenuPage */}
-        {totalItems > 0 && (
-          <div className='fixed bottom-4 right-4 bg-yellow-500 text-white
-                        rounded-full px-4 py-2 font-bold shadow-lg'>
-            Comanda: {totalItems} items
-          </div>
-        )}
-      </nav>
+      </div>
     </div>
   )
 }
